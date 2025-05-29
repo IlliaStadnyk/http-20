343c2dfde32943719d78bdecfbd53f0306bc87cb
Illia Stadnyk <ilya.stadnyk@gmail.com>
2025-05-29 20:30:34 +0200

    Test http 2.0

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+
+const x = 21;
+const app = express();
+
+app.use((req, res, next) => {
+    res.show = (name) => {
+        res.sendFile(path.join(__dirname, `/views/${name}`));
+    };
+    next();
+});
+
+app.use(express.static(path.join(__dirname, '/public')));
+
+app.get('/', (req, res) => {
+    res.show('home.html');
+});
+
+app.get('/about', (req, res) => {
+    res.show('about.html');
+});
+
+app.use('/user', (req, res) => {
+    res.show('forbidden.html');
+});
+
+app.use((req, res) => {
+    res.status(404).show('404.html');
+});
+
+app.listen(8080, () => {
+    console.log('Listening on 8080');
+});
